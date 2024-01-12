c028c2183d8567bc9a9f00628d63f97ac15824e5
LQC <boyrovip1539@gmail.com>
2024-01-12 13:34:52 +0700

    5th push

--- form01.php
+++ form01.php
@@ -1,24 +1,39 @@
 <?php
 
-include_once("functions.php");
-$txtTenSP = "";
-$txtDonGia = "";
-$cboLoaiSP = "";
-$radAnHien = 1;
-$hinh = "";
-if (isset($_POST["btnGhi"])) {
-    $txtTenSP = $_POST["txtTenSP"];
-    $txtDonGia = $_POST["txtDonGia"];
-    $cboLoaiSP = $_POST["cboLoaiSP"];
-    $radAnHien = $_POST["radAnHien"];
-    $picSP = $_FILES["picSP"];
-    $hinh = $picSP["name"];
-    move_uploaded_file($picSP["tmp_name"], "img/$hinh");
-    ghiSP($txtTenSP,$txtDonGia,$cboLoaiSP,$radAnHien,$hinh);
+    include_once("functions.php");
+    $txtTenSP = "";
+    $txtDonGia = "";
+    $cboLoaiSP = "";
+    $radAnHien = 1;
+    $hinh = "";
+    // $errTenSP = "";
+    // $errDonGia = "";
+    $errArr = ['tenSP'=>'','donGia'=>''];
+    if (isset($_POST["btnGhi"])) {
+        // var_dump($_POST);
+        $txtTenSP = $_POST["txtTenSP"];
+        $txtDonGia = $_POST["txtDonGia"];
+        $cboLoaiSP = $_POST["cboLoaiSP"];
+        $radAnHien = $_POST["radAnHien"];
+        $picSP = $_FILES["picSP"];
+        $hinh = $picSP["name"];
 
-}
-$checkedHien = $radAnHien == 1 ? "checked" : "";
-$checkedAn = $radAnHien == 0 ? "checked" : "";
+        
+        // move_uploaded_file($picSP["tmp_name"], "img/$hinh");
+        // ghiSP($txtTenSP, $txtDonGia, $cboLoaiSP, $radAnHien, $hinh);
+
+        
+        if(empty($txtTenSP)){
+            $errArr['tenSP'] = "(*) Ten San Pham Khong Duoc Rong";
+        }
+        if(empty($txtDonGia)){
+            $errArr['donGia'] = "(*) Don Gia Khong Duoc Rong";
+        } elseif(!is_numeric($txtDonGia)){
+            $errArr['donGia'] = "(*) Don Gia khong dung kieu du lieu";
+        }
+    }
+    $checkedHien = $radAnHien == 1 ? "checked" : "";
+    $checkedAn = $radAnHien == 0 ? "checked" : "";
 
 ?>
 
@@ -28,21 +43,23 @@ $checkedAn = $radAnHien == 0 ? "checked" : "";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <title>Demo Form</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" >
 </head>
 
 <body>
     <div class="container mt-4 " style="width: 800; margin: auto;">
         <h1 class="text-center">Xử lý sản phẩm</h1>
-        <form action="xulyform01.php" method="POST" enctype="multipart/form-data">
+        <form action="" method="POST" enctype="multipart/form-data">
             <div class="form-group">
                 <label>Tên Sản phẩm</label>
-                <input type="text" class="form-control" name="txtTenSP">
+                <input type="text" class="form-control" name="txtTenSP" value="<?php echo $txtTenSP; ?>">
+                <small class="text-danger font-italic"><?php echo $errArr['tenSP']; ?></small>
             </div>
             <div class="form-group">
                 <label>Đơn Giá</label>
-                <input type="text" class="form-control" name="txtDonGia">
+                <input type="text" class="form-control" name="txtDonGia" value="<?php echo $txtDonGia; ?>">
+                <small class="text-danger font-italic"><?php echo $errArr['donGia']; ?></small>
             </div>
             <div class="form-group">
                 <label for="exampleFormControlSelect1">Chọn loại sản phẩm</label>
@@ -52,7 +69,7 @@ $checkedAn = $radAnHien == 0 ? "checked" : "";
                     while ($rowLoai = mysqli_fetch_assoc($rsLoaiSP)) {
                         $selected = $cboLoaiSP == $rowLoai["id"] ? "selected" : "";
                     ?>
-                        <option value="<?php echo $rowLoai["id"] ?>" <?php echo $selected; ?>><?php echo $rowLoai["tenloai"]; ?></option>
+                        <option value="<?php echo $rowLoai["id"]; ?>" <?php echo $selected; ?>><?php echo $rowLoai["tenloai"]; ?></option>
                     <?php } ?>
 
                 </select>
@@ -67,7 +84,7 @@ $checkedAn = $radAnHien == 0 ? "checked" : "";
             </div>
             <div class="form-group">
                 <label>Chọn hình ảnh</label>
-                <input type="file" class="form-control-file" name="picSP">
+                <input type="file" class="form-control-file" name="picSP" value="abc">
             </div>
             <div class="text-center">
                 <input type="submit" class="btn btn-primary" name="btnGhi" value="Ghi Mới">
